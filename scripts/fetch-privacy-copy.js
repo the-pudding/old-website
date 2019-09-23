1e9f3a808c98499d6bbf89993a5ad222e6d84824
russellgoldenberg <russellgoldenberg@gmail.com>
2019-09-23 09:31:04 -0400

    privacy policy

---
+++ scripts/fetch-privacy-copy.js
@@ -0,0 +1,29 @@
+const archieml = require('archieml');
+const request = require('request');
+const fs = require('fs');
+
+const cwd = process.cwd();
+
+console.log('fetching privacy page copy...');
+
+const google = {
+  id: '1LToR_WZ-E8q6FMzFnYN62IfoZd6tmW80bC0kQBEGp84',
+  filename: 'privacy-copy'
+};
+
+const url = `https://docs.google.com/document/d/${google.id}/export?format=txt`;
+
+request(url, function(error, response, body) {
+  const parsed = archieml.load(body);
+  const str = JSON.stringify(parsed);
+  const basePath = `${cwd}/.tmp/data`;
+  const file = `${basePath}/${google.filename || 'copy'}.json`;
+
+  fs.writeFile(file, str, err => {
+    if (err) console.error(err);
+    console.log('DONE: fetch-privacy-copy.js');
+    process.exit();
+  });
+});
+
+
