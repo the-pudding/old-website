77743655fb5053fbe9fc94cf42134b99c190aeef
russellgoldenberg <russellgoldenberg@gmail.com>
2021-02-18 15:13:12 -0500

    fix backlog

--- templates/backlog/partials/table.js
+++ templates/backlog/partials/table.js
@@ -6,36 +6,29 @@ const backlogData = JSON.parse(
   fse.readFileSync(`${cwd}/.tmp/data/backlog.json`, 'utf-8')
 );
 
-const cols = [
-	"date",
-	"story",
-	"involvement",
-	"description",
-	"seeking",
-]
+const cols = ['date', 'story', 'description'];
 
 function createTD(d) {
-	return cols.map(c => {
-    if (!d[c]) return false
-    if (c === 'seeking' && d[c] === "TRUE") return false
-    const pre = c === 'seeking' ? '<span>Looking for: </span>' : '<span></span>'
-    const cl = c === 'involvement' ? `is-${d[c]}` : ''
-    return `<p class='idea--${c} ${cl}'>${pre}${d[c]}</p>`
-  }).filter(d => d).join('')
+  return cols
+    .map(c => {
+      if (!d[c]) return false;
+      return `<p class='idea--${c}'>${d[c]}</p>`;
+    })
+    .filter(d => d)
+    .join('');
 }
 
 function createHTML() {
-	const seekingData = backlogData.filter(d => d.public === 'TRUE' && d.seeking)
-	const regularData = backlogData.filter(d => d.public === 'TRUE' && !d.seeking)
-	const seekingHTML = seekingData.map(d => `<div class='idea is-seeking'>${createTD(d)}</div>`)
-	.join('');
-	const regularHTML = regularData.map(d => `<div class='idea'>${createTD(d)}</div>`)
-		.join('');
+  const regularData = backlogData.filter(
+    d => d.public === 'TRUE' && !d.seeking
+  );
+  const regularHTML = regularData
+    .map(d => `<div class='idea'>${createTD(d)}</div>`)
+    .join('');
 
-	return `
-		<div class='ideas--seeking'>${seekingHTML}</div>
+  return `
 		<div class='ideas--regular'>${regularHTML}</div>
-	`
+	`;
 }
 
 module.exports = function() {
